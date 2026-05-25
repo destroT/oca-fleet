58f35f64e7308e95c97748039d0826061d179dba
Tommaso Destro <destro.tommaso@gmail.com>
2026-05-25 12:32:03 +0000

    [MIG] fleet_vehicle_ownership: Migration to 15.0

--- fleet_vehicle_ownership/__manifest__.py
+++ fleet_vehicle_ownership/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Fleet Vehicle Ownership",
     "summary": "Add vehicle ownership, linking partners to vehicles",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "RPSJR,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/fleet",
